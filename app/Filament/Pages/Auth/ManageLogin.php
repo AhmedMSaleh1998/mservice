723acb9888648379766d67b9dddb58e3fba0a198
Ahmed Saleh <a-saleh@adilla.com.sa>
2026-02-21 15:42:41 +0200

    commit

--- app/Filament/Pages/Auth/ManageLogin.php
+++ app/Filament/Pages/Auth/ManageLogin.php
@@ -3,10 +3,30 @@
 namespace App\Filament\Pages\Auth;
 
 use Filament\Auth\Pages\Login;
+use Illuminate\Contracts\Support\Htmlable;
+use Illuminate\Support\HtmlString;
 
 class ManageLogin extends Login
 {
     protected array $extraBodyAttributes = [
         'class' => 'manage-login',
     ];
+
+    public function hasLogo(): bool
+    {
+        return false;
+    }
+
+    public function getHeading(): string | Htmlable | null
+    {
+        if (filled($this->userUndertakingMultiFactorAuthentication)) {
+            return parent::getHeading();
+        }
+
+        return new HtmlString(sprintf(
+            '<div class="manage-login-brand"><img src="%s" alt="%s" class="manage-login-brand-logo" loading="lazy" decoding="async"></div>',
+            e(asset('assets/ems-logo.png')),
+            e(__('Registration Request')),
+        ));
+    }
 }
